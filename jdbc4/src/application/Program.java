5d6eaae429d8192845c86b60bb51eacdf0b49c48
Marco <125201711+MarcoShvy@users.noreply.github.com>
2023-10-05 22:21:52 -0300

    Update data

---
+++ jdbc4/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+import db.DB;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+    try {
+        conn = DB.getConnection();
+
+        st = conn.prepareStatement("update seller set BaseSalary = BaseSalary + ? where(DepartmentId = ?)");
+
+        st.setDouble(1, 200);
+        st.setInt(2, 2);
+        int rows = st.executeUpdate();
+
+        System.out.println("Done! new rows = "+ rows);
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    }
+}
