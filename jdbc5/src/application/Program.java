df96b737976a3a60b35d9dd8b797c8e12222ef52
Marco <125201711+MarcoShvy@users.noreply.github.com>
2023-10-05 22:23:49 -0300

    Delete data

---
+++ jdbc5/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+import db.DB;
+import db.DbIntegrityException;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+    try {
+        conn = DB.getConnection();
+
+        st = conn.prepareStatement("DELETE FROM department WHERE(ID = ?)");
+        st.setInt(1, 6);
+
+        int rows = st.executeUpdate();
+
+        System.out.println("Done! new rows = "+ rows);
+    } catch (SQLException e) {
+        throw new DbIntegrityException(e.getMessage());
+    }
+    }
+}
